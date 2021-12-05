3fd5fc253736ae0847084e09f5c4f00f59904570
Code Sandbox <hello@codesandbox.io>
2021-12-05 11:22:43 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8879f5ca65df1a549e091e651ba0695c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8f7354cf0a61269af1872e69dea804",
+    "url": "https://yaman0007.github.io/csb-y32hm/index.html"
+  },
+  {
+    "revision": "43e7f27ef6c57e23fe38",
+    "url": "https://yaman0007.github.io/csb-y32hm/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "43e7f27ef6c57e23fe38",
+    "url": "https://yaman0007.github.io/csb-y32hm/static/js/2.488d5a42.chunk.js"
+  },
+  {
+    "revision": "44a1af8298c289492666",
+    "url": "https://yaman0007.github.io/csb-y32hm/static/js/main.94c03741.chunk.js"
+  },
+  {
+    "revision": "a0576fd2a9ef140585bd",
+    "url": "https://yaman0007.github.io/csb-y32hm/static/js/runtime-main.79cacc03.js"
+  }
+]);
